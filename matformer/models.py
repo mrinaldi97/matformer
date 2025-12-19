21605260a451ba27aec2cb197abed776f137173c
Matteo Rinaldi <matteo.rinaldi@unito.it>
2025-12-19 13:04:35 +0100

    muonclip

--- matformer/models.py
+++ matformer/models.py
@@ -277,7 +277,7 @@ class PL_ModelWrapper(MatformerModule):
             ))
             
             muon_config = MuonConfig(
-                lr=base_lr ###To check!!!
+                lr=base_lr, ###To check!!!
                 muon_beta=self.train_config.get("muon_momentum", 0.95),
                 muon_decay=self.train_config.get("weight_decay", 0.01),
                 ns_steps=self.train_config.get("muon_ns_steps", 5),
