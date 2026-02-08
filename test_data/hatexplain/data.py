3bb099a42d62de5684a1c4bd715c00a81fe49390
Jj-source <jacopo.gili@unito.it>
2026-02-08 14:35:27 +0000

    updated data + config

--- test_data/hatexplain/data.py
+++ test_data/hatexplain/data.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import Counter
 
 def process_hatexplain(folder, data_path, split_path):
     with open(folder+data_path, 'r') as f:
@@ -24,6 +25,12 @@ def process_hatexplain(folder, data_path, split_path):
                 # Keep the original post_id for reference
                 entry['post_id'] = post_id
                 
+                # take the max of annotator label
+                labels = [label_converter(ann['label']) for ann in entry['annotators']]
+                entry['final_label'] = Counter(labels).most_common(1)[0][0]
+                
+                del entry['annotators']
+                
                 processed_splits[split_name].append(entry)
 
     for split_name, data in processed_splits.items():
@@ -31,6 +38,15 @@ def process_hatexplain(folder, data_path, split_path):
         with open(folder+output_filename, 'w') as f:
             json.dump(data, f, indent=4)
         print(f"Saved {len(data)} samples to {output_filename}")
+
+# hatespeech (0), normal (1) or offensive (2)
+def label_converter(label):
+  label_dict = {
+    'hatespeech':0,
+    'normal':1,
+    "offensive":2
+  }
+  return label_dict[label]
 
 if __name__ == "__main__":
     process_hatexplain('test_data/hatexplain/', 'dataset.json', 'post_id_divisions.json')
